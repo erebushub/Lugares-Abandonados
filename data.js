90e64e8047d231f3d56a5ceb094a2d8ee3d9ce07
erebushub <erebushub@tutamail.com>
2026-07-31 16:31:36 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,16 @@
+const lugares = [
+  {
+    id: "esp-yera",
+    nombre: "Estación abandonada de Yera",
+    ubicacion: "Vega de Pas, Cantabria",
+    tipo: "Estaciones",
+    anio: "Siglo XX",
+    descripcion:
+      "Antigua estación de la línea Santander-Mediterráneo, situada junto al túnel de La Engaña.",
+    imagen:
+      "https://commons.wikimedia.org/wiki/Special:Redirect/file/Disused_station_of_Yera.jpg"
+  },
+
+  // ...todos los demás que ya tienes...
+
+];
