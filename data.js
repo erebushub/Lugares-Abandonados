c39674d8b72e1388c4575853b9fbaae74462c8a5
erebushub <erebushub@tutamail.com>
2026-07-31 16:44:18 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,34 @@
-const lugares = [];
+const lugares = [
+  {
+    pais: "Italia",
+    zonas: [
+      {
+        nombre: "Lombardía",
+        ciudades: [
+          {
+            nombre: "Milán",
+            lugares: []
+          }
+        ]
+      },
+      {
+        nombre: "Lacio",
+        ciudades: [
+          {
+            nombre: "Roma",
+            lugares: []
+          }
+        ]
+      },
+      {
+        nombre: "Campania",
+        ciudades: [
+          {
+            nombre: "Nápoles",
+            lugares: []
+          }
+        ]
+      }
+    ]
+  }
+];
